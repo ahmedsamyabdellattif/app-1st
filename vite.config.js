69b0eddd21988322a16e9bb7d6c0bea3170e5fb6
ahmed <ahmed.samy.abdellattif@gmail.com>
2025-07-29 22:21:54 +0300

    fix: set base to relative path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // أو '/' لو المشروع في الدومين الرئيسي
+  base: './',
   plugins: [react()],
 })
