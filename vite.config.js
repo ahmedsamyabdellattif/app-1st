8fc834d954e7369e95d8de93a5c4b9f10469e173
ahmed <ahmed.samy.abdellattif@gmail.com>
2025-07-29 22:18:06 +0300

    fix: set correct base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/App-1st/',
+  base: './', // أو '/' لو المشروع في الدومين الرئيسي
   plugins: [react()],
 })
